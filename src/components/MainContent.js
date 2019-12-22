853735b9549fe1ce163261aa57e4b9e7531028d2
Gaurav KC <13gaurab@gmail.com>
2019-12-22 22:00:19 +0545

    Add loading state and format file

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,74 +1,108 @@
-import React, { useRef } from 'react';
-import Card from 'react-bootstrap/Card';
-import { useFile } from 'react-blockstack';
+import React, { useRef } from "react";
+import Card from "react-bootstrap/Card";
+import { useFile } from "react-blockstack";
 import { Link } from "react-router-dom";
 
-export default function MainContent({walletPath}) {
+export default function MainContent({ walletPath }) {
   const [credentials, setCredentials] = useFile(`${walletPath}.json`);
-  const title = useRef('');
-  const address = useRef('');
+  const title = useRef("");
+  const address = useRef("");
   const handleClick = () => {
-    if ( title.current.value.length && address.current.value.length ) {
-      const newCred = { walletName: title.current.value, walletAddress: address.current.value };
+    if (title.current.value.length && address.current.value.length) {
+      const newCred = {
+        walletName: title.current.value,
+        walletAddress: address.current.value
+      };
       const oldCred = credentials ? JSON.parse(credentials) : [];
-      setCredentials(JSON.stringify([ ...oldCred, newCred ]));
+      setCredentials(JSON.stringify([...oldCred, newCred]));
     }
-  }
+  };
+
   return (
     <>
       <div className="row mt-3">
         <div className="col-12 py-1">
-          <Link to='/'>
-            <span className="p-2 border"><i className="fa fa-arrow-left"></i> Back</span>
+          <Link to="/">
+            <span className="p-2 border">
+              <i className="fa fa-arrow-left"></i> Back
+            </span>
           </Link>
           <span className="ml-3 font-weight-bold text-uppercase">{`${walletPath} Wallets`}</span>
         </div>
       </div>
-      <div className="row mt-3">
-        <div className="d-flex col-12">
-          {credentials ? JSON.parse(credentials).map( credential => {
-            return (
-              <Card style={{ width: "17rem" }} className="mr-2" key={credential.walletAddress}>
-                <Card.Body>
-                  <Card.Title>{credential.walletName}</Card.Title>
-                  <Card.Text>{credential.walletAddress}</Card.Text>
-                </Card.Body>
-              </Card>
-            );
-          }) : <div>0 Wallets</div>}
-        </div>
-      </div>
-      <br />
-      <div className="row">
-        <form className="col-12">
-          <div className="form-group row">
-            <label htmlFor="inputName" className="col-sm-2 col-form-label">
-              Wallet Name
-            </label>
-            <div className="col-4">
-              <input type="text" ref={title} className="form-control" id="inputName" />
+      {credentials === undefined ? (
+        <div className="mt-3">Loading..</div>
+      ) : (
+        <>
+          <div className="row mt-3">
+            <div className="d-flex col-12">
+              {credentials === null ? (
+                <div>0 Wallets</div>
+              ) : (
+                JSON.parse(credentials).map(credential => {
+                  return (
+                    <Card
+                      style={{ width: "17rem" }}
+                      className="mr-2"
+                      key={credential.walletAddress}
+                    >
+                      <Card.Body>
+                        <Card.Title>{credential.walletName}</Card.Title>
+                        <Card.Text>{credential.walletAddress}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  );
+                })
+              )}
             </div>
           </div>
-          <div className="form-group row">
-            <label htmlFor="inputAddress" className="col-sm-2 col-form-label">
-              Wallet Address
-            </label>
-            <div className="col-4">
-              <input
-                type="text"
-                className="form-control"
-                id="inputAddress"
-                ref={address}
-              />
-            </div>
+          <br />
+          <div className="row">
+            <form className="col-12">
+              <div className="form-group row">
+                <label htmlFor="inputName" className="col-sm-2 col-form-label">
+                  Wallet Name
+                </label>
+                <div className="col-4">
+                  <input
+                    type="text"
+                    ref={title}
+                    className="form-control"
+                    id="inputName"
+                  />
+                </div>
+              </div>
+              <div className="form-group row">
+                <label
+                  htmlFor="inputAddress"
+                  className="col-sm-2 col-form-label"
+                >
+                  Wallet Address
+                </label>
+                <div className="col-4">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputAddress"
+                    ref={address}
+                  />
+                </div>
+              </div>
+              <div className="form-group row">
+                <div className="col-sm-10">
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={handleClick}
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-          <div className="form-group row">
-            <div className="col-sm-10">
-              <button type="button" className="btn btn-primary" onClick={handleClick}>Save</button>
-            </div>
-          </div>
-        </form>
-      </div>
+        </>
+      )}
     </>
   );
 }
